94a15d77856259412af6093466087d60c338ebd2
stlbucket <stlbucket@gmail.com>
2018-05-06 16:03:28 -0500

    get_job

--- db/work_queue.sql
+++ db/work_queue.sql
@@ -116,24 +116,24 @@
 -- END;
 -- $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION app_jobs.fail_job(worker_id varchar, job_id int, error_message varchar) RETURNS app_jobs.job AS $$
-DECLARE
-  v_row app_jobs.job;
-BEGIN
-  UPDATE app_jobs.job
-    SET
-      last_error = error_message,
-      run_at = greatest(now(), run_at) + (exp(least(attempts, 10))::text || ' seconds')::interval
-    WHERE id = job_id
-    RETURNING * INTO v_row;
+-- CREATE FUNCTION app_jobs.fail_job(worker_id varchar, job_id int, error_message varchar) RETURNS app_jobs.job AS $$
+-- DECLARE
+--   v_row app_jobs.job;
+-- BEGIN
+--   UPDATE app_jobs.job
+--     SET
+--       last_error = error_message,
+--       run_at = greatest(now(), run_at) + (exp(least(attempts, 10))::text || ' seconds')::interval
+--     WHERE id = job_id
+--     RETURNING * INTO v_row;
 
-  UPDATE app_jobs.job_queue
-    SET locked_by = null, locked_at = null
-    WHERE queue_name = v_row.queue_name AND locked_by = worker_id;
+--   UPDATE app_jobs.job_queue
+--     SET locked_by = null, locked_at = null
+--     WHERE queue_name = v_row.queue_name AND locked_by = worker_id;
 
-  RETURN v_row;
-END;
-$$ LANGUAGE plpgsql;
+--   RETURN v_row;
+-- END;
+-- $$ LANGUAGE plpgsql;
 
 CREATE FUNCTION app_jobs.get_job(worker_id varchar, identifiers varchar[]) RETURNS app_jobs.job AS $$
 DECLARE
