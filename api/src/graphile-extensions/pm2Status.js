74d68520dedd636b2881539f252055060538fb71
stlbucket <stlbucket@gmail.com>
2018-08-22 15:06:37 -0700

    pm2

---
+++ api/src/graphile-extensions/pm2Status.js
@@ -0,0 +1,66 @@
+const pm2 = require("pm2")
+const Promise = require('bluebird')
+const { makeExtendSchemaPlugin, gql } = require("graphile-utils");
+
+const Pm2ProcessesPlugin = makeExtendSchemaPlugin(build => {
+  const { pgSql: sql } = build;
+  return {
+    typeDefs: gql`
+    type SyncProcess {
+      pid: String!
+      name: String!
+      status: String!
+    }
+
+    type SyncProcessCollection {
+      nodes: [SyncProcess]!
+    }
+
+    extend type Query {
+      syncProcesses: SyncProcessCollection!
+    }
+  `,
+  resolvers: {
+      Query: {
+        syncProcesses: async (
+          _query,
+          args,
+          context,
+          resolveInfo,
+          { selectGraphQLResultFromTable }
+        ) => {
+          const d = Promise.defer()
+
+          pm2.connect(function(err) {
+            if (err) {
+              console.error(err);
+              process.exit(2);
+            }
+            
+            pm2.list({
+            }, function(err, apps) {
+              pm2.disconnect();   // Disconnects from PM2
+
+              if (err) d.reject(err)
+              else {
+                d.resolve({
+                  nodes: apps.map(
+                  p => {
+                    return {
+                      pid: p.pid
+                      ,name: p.name
+                      ,status: p.pm2_env.status
+                    }
+                })})  
+              }
+            })
+          })
+
+          return d.promise
+        },
+      },
+    },
+  };
+});
+
+module.exports = Pm2ProcessesPlugin
